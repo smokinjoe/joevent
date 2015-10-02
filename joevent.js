632b55e85dd0c0e475c2033164e76fed448b9f8c
Joe <joe.ekiert@gmail.com>
2015-10-02 06:59:21 +0000

    bunch of renaming, snake_case => camelCase

--- joevent.js
+++ joevent.js
@@ -1,18 +1,18 @@
-var joevent = (function () {
+var j = (function () {
   // private vars
   var debug = false;
   var hooks = {};
 
   // private methods
-  var _call_hook = function (method, args, context) {
-    args.push(context);  // should this be here or in fire_event() ?
+  var _callHhook = function (method, args, context) {
+    args.push(context);  // should this be here or in fire() ?
     method.apply(window, args);
   };
 
   // public methods
   return {
-    set_hook: function (e, name, method, args) {
-      if (debug) console.log('set_hook: ' + name + ' for ' + e);
+    set: function (e, name, method, args) {
+      if (debug) console.log('set: ' + name + ' for ' + e);
 
       hooks[e] = hooks[e] || {};
       hooks[e][name] = hooks[e][name] || [];
@@ -22,22 +22,22 @@ var joevent = (function () {
         'args' : (typeof args === Array) ? args : [args] // this needs to be an array
       };
     },
-    remove_hook: function (e, name) {
-      if (debug) console.log('remove_hook: ' + arg);
+    remove: function (e, name) {
+      if (debug) console.log('remove: ' + arg);
 
       if (typeof hooks[e][name] !== undefined) {
         delete hooks[e][name];
       }
     },
-    fire_event: function (e, context) {
-      if (debug) console.log('fire_event: ' + e);
+    fire: function (e, context) {
+      if (debug) console.log('fire: ' + e);
 
       var context = context || self;
 
       if (typeof hooks[e] !== undefined) {
         for (var key in hooks[e]) {
           var hook = hooks[e][key];
-          _call_hook(hook.method, hook.args, context);
+          _callHook(hook.method, hook.args, context);
         }
       }
     },
@@ -47,4 +47,4 @@ var joevent = (function () {
   };
 }());
 
-//joevent.set_hook('the_event', 'hook_name', f(), args);
+//joevent.set('the_event', 'hook_name', f(), args);
